83da7ed459c28fc0d398db22056e9bab15be13fb
Edward <eduard.tymoshuk@gmail.com>
2024-01-07 17:26:41 +0100

    small changes of  404 page

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,5 @@
-import Image from "next/image"
 import Link from "next/link"
+import { IoCaretBack } from "react-icons/io5"
 
 /**
  * Page404 component for rendering a custom 404 page.
@@ -8,20 +8,17 @@ import Link from "next/link"
  */
 const Page404 = () => {
     return (
-        <div className="flex items-center justify-center h-screen bg-black">
+        <div className="flex items-center justify-center h-screen bg-[url('/404-page-bg.svg')]">
             <div className="text-center">
                 <h1 className="text-4xl text-secondary-color">404 | Page not found</h1>
-                <div className="flex justify-center py-4">
-                    <Image src="/astronaut.png" alt="astronaut" width={200} height={200} />
-                </div>
-                <div className="flex flex-col gap-2 items-center">
+                <d iv className="flex flex-col gap-8 items-center">
                     <h2 className="text-2xl text-white">Oops! It looks like you're lost.</h2>
                     <button>
-                        <Link href="/" className="text-white">
-                            Back Home
+                        <Link href="/" className="text-white flex items-center">
+                             <IoCaretBack /> Back home
                         </Link>
                     </button>
-                </div>
+                </d>
             </div>
         </div>
     )
